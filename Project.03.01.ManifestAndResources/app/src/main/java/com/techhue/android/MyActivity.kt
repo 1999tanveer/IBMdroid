1a3595249d1155f41fc744418449a0c74d77dac5
TANVEER <1999tanveer@gmail.com>
2020-03-05 14:57:02 +0530

    Update MyActivity.kt

--- Project.03.01.ManifestAndResources/app/src/main/java/com/techhue/android/MyActivity.kt
+++ Project.03.01.ManifestAndResources/app/src/main/java/com/techhue/android/MyActivity.kt
@@ -5,7 +5,7 @@ class MyActivity:Activity() {
     /**
      * Called when the activity is first created.
      */
-    override fun onCreate(savedInstanceState:Bundle) {
+    override fun onCreate(savedInstanceState:Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.reslayout)
     }
